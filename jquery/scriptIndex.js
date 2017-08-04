594829916d1a4138d1da5c8ef93c7a9a1903e8d0
root <jesusdavid2405@gmail.com>
2017-08-04 19:48:28 -0400

    Modificaciones en la seccion Noticias, efectos css y color del background

--- jquery/scriptIndex.js
+++ jquery/scriptIndex.js
@@ -58,6 +58,22 @@ $(document).ready(function()
 
   $('.parallax').parallax();
 
+  /* Boton de Ir Hacia Arriba */
+
+  $('.ir-arriba').click(function(){
+    $('body, html').animate({
+      scrollTop: '0px'
+    }, 300);
+  });
+ 
+  $(window).scroll(function(){
+    if( $(this).scrollTop() > 0 ){
+      $('.ir-arriba').slideDown(300);
+    } else {
+      $('.ir-arriba').slideUp(300);
+    }
+  });
+
 });
 
 
